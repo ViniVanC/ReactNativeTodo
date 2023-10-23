092444b854fffb0a958c3a8d4dadaa00a1041e85
ViniVanC <vincentvan0302@gmail.com>
2023-10-23 16:42:14 +0300

    commit

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
-import { useState } from "react";
-import { FlatList, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { FlatList, StyleSheet, Text, View } from "react-native";
 import Header from "./components/Header";
 import ListItem from "./components/ListItem";
 import Form from "./components/Form";
@@ -7,42 +8,88 @@ import Form from "./components/Form";
 export default function App() {
   const [listOfItems, setListOfItems] = useState([]);
 
-  const addHandler = (text) => {
-    setListOfItems((list) => {
-      if (text !== "") {
-        return [
-          {
-            done: false,
-            text: text,
-            key: Math.random().toString(36).substring(7),
-          },
-          ...list,
-        ];
-      } else {
-        return [...list];
+  useEffect(() => {
+    const loadItems = async () => {
+      try {
+        // Отримуємо список елементів з локального сховища
+        const storedList = await AsyncStorage.getItem("todolist");
+        if (storedList) {
+          const parsedList = JSON.parse(storedList);
+          setListOfItems(parsedList);
+        }
+      } catch (error) {
+        console.error("Помилка завантаження даних: ", error);
       }
-    });
+    };
+
+    loadItems();
+  }, []);
+
+  const addHandler = async (text) => {
+    if (text !== "") {
+      const newItem = {
+        done: false,
+        text: text,
+        key: Math.random().toString(36).substring(7),
+      };
+
+      try {
+        // Отримуємо поточний список елементів з локального сховища
+        const storedList = await AsyncStorage.getItem("todolist");
+        const currentList = storedList ? JSON.parse(storedList) : [];
+
+        // Додаємо новий елемент до списку
+        currentList.push(newItem);
+
+        // Зберігаємо оновлений список в локальному сховищі
+        await AsyncStorage.setItem("todolist", JSON.stringify(currentList));
+
+        // Оновлюємо стан компонента
+        setListOfItems(currentList);
+      } catch (error) {
+        console.error("Помилка збереження даних: ", error);
+      }
+    }
   };
 
-  const deleteItem = (key) => {
-    setListOfItems((list) => list.filter((item) => item.key !== key));
+  const deleteItem = async (key) => {
+    const updatedTasks = listOfItems.filter((item) => item.key !== key);
+
+    try {
+      // Зберігаємо оновлений список в локальному сховищі
+      await AsyncStorage.setItem("todolist", JSON.stringify(updatedTasks));
+
+      // Оновлюємо стан компонента
+      setListOfItems(updatedTasks);
+    } catch (error) {
+      console.error("Помилка збереження даних: ", error);
+    }
   };
 
-  const doneHandler = (key) => {
+  const doneHandler = async (key) => {
     const updatedTasks = listOfItems.map((task) => {
       if (task.key === key) {
         return { ...task, done: !task.done };
       }
       return task;
     });
-    setListOfItems(updatedTasks);
+
+    try {
+      // Зберігаємо оновлений список в локальному сховищі
+      await AsyncStorage.setItem("todolist", JSON.stringify(updatedTasks));
+
+      // Оновлюємо стан компонента
+      setListOfItems(updatedTasks);
+    } catch (error) {
+      console.error("Помилка збереження даних: ", error);
+    }
   };
 
   return (
     <View style={styles.main}>
       <Header />
       <Form addHandler={addHandler} />
-      <View style={{ flex: 1, paddingBottom: 20 }}>
+      <View style={{ flex: 1, paddingBottom: 25 }}>
         <FlatList
           data={listOfItems}
           renderItem={({ item }) => (
